facc092b94bf91172dc7fd08c696b7dc87c89381
theoc22 <theochatkin@protonmail.com>
2026-05-28 21:33:23 -0300

    correcao

--- leetcode/arrays/minimumelementsum.c
+++ leetcode/arrays/minimumelementsum.c
@@ -1,3 +1,23 @@
 int minElement(int* nums, int numsSize) {
-    
+    int somas[numsSize];
+    int menor = 100000;
+
+    for ( int i = 0; i < numsSize; i++){
+        int soma = 0;
+        
+        while ( nums[i] != 0 ){
+            soma += nums[i] % 10;
+            nums[i] /= 10;
+        }
+        somas[i] = soma;
+    }
+
+    for ( int i = 0; i < somas; i++){
+        if ( somas[i] < menor){
+            menor = somas[i];
+        }
+    }
+
+    return menor;
+
 }
